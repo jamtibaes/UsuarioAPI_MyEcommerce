7264d7136e07eeee4e2391ff27bfbf674b35ada3
jamtibaes <jamtibaes@yahoo.com.br>
2022-05-21 19:00:45 -0300

    Initial commit

---
+++ src/main/java/com/letscode/ecommerce/usuarioapi/EcommerceUsuarioApiApplication.java
@@ -0,0 +1,13 @@
+package com.letscode.ecommerce.usuarioapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceUsuarioApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EcommerceUsuarioApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/letscode/ecommerce/usuarioapi/EcommerceUsuarioApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.letscode.ecommerce.usuarioapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceUsuarioApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
