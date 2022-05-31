084dc2fab463225f51cfc45d0ed559bb944ce720
jamtibaes <jamtibaes@yahoo.com.br>
2022-05-31 20:13:08 -0300

    API MVP

--- src/main/java/com/letscode/ecommerce/userapi/entity/UserEntity.java
+++ src/main/java/com/letscode/ecommerce/userapi/entity/UserEntity.java
@@ -1,9 +1,6 @@
 package com.letscode.ecommerce.userapi.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class UserEntity {
